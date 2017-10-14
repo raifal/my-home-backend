358294c8899798f18ef808bb05b4209594bcccb6
Rainer Faller <mail@rainer-faller.de>
2017-10-14 21:55:55 +0200

    added hint about host file

--- start_development_database.ps1
+++ start_development_database.ps1
@@ -4,6 +4,15 @@ foreach ($line in $lines) {
     Set-Variable $a $b
 }
 
+
+$ip=docker-machine ip
+Write-Host ""
+Write-Host ""
+Write-Host "Make sure to have the following entry in the C:\Windows\System32\drivers\etc\hosts" -foreground Yellow
+Write-Host "$ip   hsm-database" -foreground Yellow
+Write-Host ""
+Write-Host ""
+
 docker stop hsm-database
 docker rm -v hsm-database
 docker run --name hsm-database -e POSTGRES_PASSWORD=${postgres_password} -p 5432:5432 -d postgres:10.0
