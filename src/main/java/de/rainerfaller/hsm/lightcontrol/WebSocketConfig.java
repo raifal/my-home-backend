5999bf0b7e497085df37c1b7c741e0ae8b2cb9bc
Rainer Faller <raifal@users.noreply.github.com>
2017-11-16 06:11:41 +0100

    added websocket support

--- src/main/java/de/rainerfaller/hsm/lightcontrol/WebSocketConfig.java
+++ src/main/java/de/rainerfaller/hsm/lightcontrol/WebSocketConfig.java
@@ -6,6 +6,9 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.server.RequestUpgradeStrategy;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -15,13 +18,19 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry config) {
         ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
         taskScheduler.afterPropertiesSet();
-        config.enableSimpleBroker("/topic").setTaskScheduler(taskScheduler).setHeartbeatValue(new long[]{5000,5000});
+        config.enableSimpleBroker("/topic").setTaskScheduler(taskScheduler).setHeartbeatValue(new long[]{5000, 5000});
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/hsm/lightcontrol-websocket").withSockJS();
+        //registry.addEndpoint("/hsm/lightcontrol-websocket").withSockJS();
+
+        RequestUpgradeStrategy upgradeStrategy = new TomcatRequestUpgradeStrategy();
+        registry.addEndpoint("/hsm/lightcontrol-websocket")
+                .setHandshakeHandler(new DefaultHandshakeHandler(upgradeStrategy))
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 
 }
