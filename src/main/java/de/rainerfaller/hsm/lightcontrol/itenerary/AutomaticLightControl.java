bbb1e484c2442c2dc6912f77e7b52bb0caa1fe67
Rainer Faller <raifal@users.noreply.github.com>
2017-12-02 22:43:05 +0100

    automatic lights draft time table

--- src/main/java/de/rainerfaller/hsm/lightcontrol/itenerary/AutomaticLightControl.java
+++ src/main/java/de/rainerfaller/hsm/lightcontrol/itenerary/AutomaticLightControl.java
@@ -3,13 +3,14 @@ package de.rainerfaller.hsm.lightcontrol.itenerary;
 import de.rainerfaller.hsm.controller.ui.HomeAwayController;
 import de.rainerfaller.hsm.controller.ui.LightController;
 import de.rainerfaller.hsm.controller.ui.dto.Light;
-import de.rainerfaller.hsm.dao.HomeAwayRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalTime;
 
 @Configuration
 @EnableScheduling
@@ -36,8 +37,28 @@ public class AutomaticLightControl {
         else
         {
             // not at home, so enable automatic light control
-            lightController.lightOn("8");
+            LocalTime now = LocalTime.now();
+            logger.info("now is: " + now);
+
+            if ( inBetween(now,"21:40-21:42"))
+            {
+                lightController.lightOn("8");
+            }
+            if ( inBetween(now,"21:43-21:45"))
+            {
+                lightController.lightOn("8");
+            }
+            lightController.lightOff("8");
         }
 
+    }
+
+    private boolean inBetween(LocalTime now, String between) {
+        int start_hour = Integer.parseInt(between.substring(0, 2));
+        int start_min = Integer.parseInt(between.substring(3, 5));
+        int end_hour = Integer.parseInt(between.substring(6, 8));
+        int end_min = Integer.parseInt(between.substring(9, 11));
+
+        return now.isAfter(LocalTime.of(start_hour, start_min)) && now.isBefore(LocalTime.of(end_hour, end_min));
     }
 }
